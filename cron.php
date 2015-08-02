9e2ab36d8f6a838b6cef6f5a4a3e977251d3c866
root <root@bifrost.n3xus.de>
2015-08-02 03:52:08 +0200

    First commit

---
+++ cron.php
@@ -0,0 +1,14 @@
+<?php
+/*
+
+	Author:		DatN3xus
+	URL: 		https://github.com/DatN3xus
+			https://n3xus.de
+	Contact:	pun1a@4players.de
+
+	Title: 		kimsufi-alert
+	Description:	Sends an email, if a specific server is available at kimsufi.com
+
+*/
+
+?>
